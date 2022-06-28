ed6fa083901c27f8d2258baabe6f46ddca5f7d76
Ivan Silva <ivankuartz@hotmail.com>
2022-06-28 15:38:07 -0300

    Make migrations

--- db/migrate/20220628183136_add_description_to_inventories_table.rb
+++ db/migrate/20220628183136_add_description_to_inventories_table.rb
@@ -1,5 +1,5 @@
 class AddDescriptionToInventoriesTable < ActiveRecord::Migration[7.0]
   def change
-    add_column(: inventories, :description, :string)  
+    add_column(:inventories, :description, :string)  
   end
 end
