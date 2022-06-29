0d89eab483037a640a83a1c2256c1775712b8a56
Saadat Ali <saadatlucky313@gmail.com>
2022-06-29 15:37:40 +0500

    Add route for viewing recipe

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/recipes/:recipe_id', to: 'recipes#show'
   # Defines the root path route ("/")
   # root "articles#index"
 end
