e999ab9da9533dfd5156e4c030f9fdf1d45e9479
Ivan Silva <ivankuartz@hotmail.com>
2022-06-30 12:30:36 -0300

    Correct linters

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -2,6 +2,7 @@ class InventoriesController < ApplicationController
   def index
     @inventories = Inventory.all
   end
+
   def show
     @inventory = Inventory.find(params[:inventory_id].to_i)
   end
