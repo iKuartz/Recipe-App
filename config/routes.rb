ad7d138a8a8d19c66b91ff1b1747ac6c0aaf01f2
Saadat Ali <saadatlucky313@gmail.com>
2022-07-01 12:18:00 +0500

    Add routes for forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "foods", to: "foods#index"
+  post "recipes/:recipe_id/ingredient/add", to: "recipes#add", as: "add_ingredient"
+  post "recipes/:recipe_id/ingredient/:id", to: "recipes#update_quantity", as: "update_quantity"
+  delete "recipes/:recipe_id/:ingredient_id", to: "recipes#remove_ingredient", as: 'delete_ingredient'
   get "recipes", to: "recipes#index"
   get "recipes/:recipe_id", to: "recipes#show"
   get "public_recipes", to: "recipes#public_recipes"
