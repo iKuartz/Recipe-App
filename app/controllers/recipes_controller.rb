fcc961189344981a5429e39ac1c07de716424c83
Saadat Ali <saadatlucky313@gmail.com>
2022-07-02 00:59:47 +0500

    Add update and destroy function

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,6 +7,7 @@ class RecipesController < ApplicationController
     @foods.each do |food|
       @food_options << [food.name, food.id]
     end
+    @user = current_user
   end
 
   def remove_ingredient
@@ -32,13 +33,51 @@ class RecipesController < ApplicationController
     recipe_food = RecipeFood.create(quantity: food_params["quantity"], recipe_id: params["recipe_id"].to_i, food_id: food_params["foods"].to_i)
     unless recipe_food.new_record?
       respond_to do |format|
-        format.html { redirect_to "/recipes/#{params[:recipe_id].to_i}" }
+        format.html { redirect_to "/recipes/#{params[:recipe_id]}" }
         format.json { head :no_content }
       end
     end
   end
 
+  def update
+    recipe = Recipe.find(params["recipe_id"].to_i)
+    if recipe_access_params[:public].to_i.zero?
+      recipe.public = false
+    else
+      recipe.public = true
+    end
+    if recipe.save
+      respond_to do |format|
+        format.html { redirect_to "/recipes/#{params[:recipe_id]}"}
+        format.json { head :no_content}
+      end
+    end
+  end
+
+  def destroy
+    recipe = Recipe.find(params["recipe_id"].to_i)
+    recipe_foods = RecipeFood.where(recipe_id: params[:recipe_id].to_i)
+    recipe_foods.delete_all
+    destroyedRecipe = recipe.destroy
+    puts destroyedRecipe
+    if destroyedRecipe.destroyed?
+      respond_to do |format|
+        format.html { redirect_to "/recipes/" }
+        format.json { head :no_content}
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to "/recipes/#{params[:recipe_id]}"}
+        format.json { head :no_content}
+      end
+    end
+  end
+
   private
+
+  def recipe_access_params
+    params.require(:access).permit(:public)
+  end
 
   def recipe_params
     params.require(:recipe_food).permit(:quantity)
