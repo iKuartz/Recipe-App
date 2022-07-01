b41764095a9ad1155f63950d0dab61bc99f55f0c
Saadat Ali <saadatlucky313@gmail.com>
2022-07-01 12:16:30 +0500

    Import bootstrap and jquery3 in application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
+//= require jquery3
+//= require bootstrap
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
