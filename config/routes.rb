248efac9629b35af0e3819f0caae8bac002f5fe2
Saadat Ali <saadatlucky313@gmail.com>
2022-07-02 01:02:39 +0500

    Add routes for updating and deleting recipe

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "foods", to: "foods#index"
+  post "recipes/:recipe_id/update_access", to: "recipes#update", as: "change_access_for_recipe"
   post "recipes/:recipe_id/ingredient/add", to: "recipes#add", as: "add_ingredient"
   post "recipes/:recipe_id/ingredient/:id", to: "recipes#update_quantity", as: "update_quantity"
+  delete "recipes/:recipe_id/delete", to: "recipes#destroy", as: "delete_recipe"
   delete "recipes/:recipe_id/:ingredient_id", to: "recipes#remove_ingredient", as: 'delete_ingredient'
   get "recipes", to: "recipes#index"
   get "recipes/:recipe_id", to: "recipes#show"
